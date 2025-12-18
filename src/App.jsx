834e09c4420ce8ff4598cc271ec2ac38449fba99
stefan5035 <stefan.madethen@gmail.com>
2025-12-18 13:10:21 +0700

    livecoding 003

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import { useState } from 'react'
 
 function App() {
-  
+  const [users, setUsers] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [department, setDepartment] = useState('');
+  const [address, setAddress] = useState('');
+  const [message, setMessage] = useState('');
+ 
   return (
     <>
     <div class="container">
-      <div class="content mt-3">
+      <div class="form-container mt-3">
         <h2>Registration Form</h2>
       </div>
       <div>
         <form>
           <label>First Name</label>
-          <input type="text" class="form-control mb-3" required/>
+          <input type="text" class="form-control mb-3" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
           <label>Last Name</label>
-          <input type="text" class="form-control mb-3" required/>
+          <input type="text" class="form-control mb-3" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
           <label>Department</label>
-          <select class="form-select mb-3" required>
+          <select class="form-select mb-3" value={department} onChange={(e) => setDepartment(e.target.value)}>
             <option selected>Please choose one</option>
             <option value="data">Data Management</option>
             <option value="admin">Finance, HR, and Administration</option>
             <option value="pdo">Product Development and Operation</option>
           </select>
           <label>Address</label>
-          <textarea rows="5" class="form-control mb-3" required></textarea>
+          <textarea rows="5" class="form-control mb-3" value={address} onChange={(e) => setAddress(e.target.value)}></textarea>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
