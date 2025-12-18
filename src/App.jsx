27c64fffd5a2a1946937f8a1fa54d1d8a4b5dcc6
stefan5035 <stefan.madethen@gmail.com>
2025-12-18 10:07:36 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  
+  return (
+    <>
+      <div class="mt-5">
+        <h2>Registration Form</h2>
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
