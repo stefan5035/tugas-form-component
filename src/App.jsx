b6b729ba4a3f7e422a129015540d1e30e04903cb
stefan5035 <stefan.madethen@gmail.com>
2025-12-18 11:43:27 +0700

    livecoding 001

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
 
 function App() {
   
   return (
     <>
-      <div class="mt-5">
+    <div class="container">
+      <div class="content mt-3">
         <h2>Registration Form</h2>
       </div>
-      
+      <div>
+        <form>
+          <label>First Name</label>
+          <input type="text" class="form-control" required/>
+          <label>Last Name</label>
+          <input type="text" class="form-control" required/>
+          <label>Department</label>
+          <input type="dropdown" class="form-control" required/>
+          <label>Address</label>
+          <textarea rows="5" class="form-control" required></textarea>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div>
+        <table></table>
+      </div>
+    </div>
       
     </>
   )
