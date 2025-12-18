9799b0979dfb3aa0f0a8c1f901e6bc9842240976
stefan5035 <stefan.madethen@gmail.com>
2025-12-18 11:44:01 +0700

    livecoding 002

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,18 @@ function App() {
       <div>
         <form>
           <label>First Name</label>
-          <input type="text" class="form-control" required/>
+          <input type="text" class="form-control mb-3" required/>
           <label>Last Name</label>
-          <input type="text" class="form-control" required/>
+          <input type="text" class="form-control mb-3" required/>
           <label>Department</label>
-          <input type="dropdown" class="form-control" required/>
+          <select class="form-select mb-3" required>
+            <option selected>Please choose one</option>
+            <option value="data">Data Management</option>
+            <option value="admin">Finance, HR, and Administration</option>
+            <option value="pdo">Product Development and Operation</option>
+          </select>
           <label>Address</label>
-          <textarea rows="5" class="form-control" required></textarea>
+          <textarea rows="5" class="form-control mb-3" required></textarea>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
